7508fb8d35818b6d41e8d0f61f990d9829410632
imba97 <mail@imba97.cn>
2025-05-22 15:43:39 +0800

    chore: update unocss config

--- uno.config.ts
+++ uno.config.ts
@@ -3,20 +3,22 @@ import {
   presetAttributify,
   presetIcons,
   presetTypography,
-  presetUno,
+  presetWind3,
   transformerDirectives,
   transformerVariantGroup
 } from 'unocss'
 
-import { presetUseful } from 'unocss-preset-useful'
-
 export default defineConfig({
   presets: [
-    presetUno(),
-    presetUseful(),
+    presetWind3(),
     presetAttributify(),
     presetIcons({
-      scale: 1.2
+      extraProperties: {
+        'display': 'inline-block',
+        'height': '1.2em',
+        'width': '1.2em',
+        'vertical-align': 'text-bottom'
+      }
     }),
     presetTypography()
   ],
@@ -33,5 +35,35 @@ export default defineConfig({
       '2xl': '1536px',
       '3xl': '1920px'
     }
-  }
+  },
+  shortcuts: [
+    [/^clickable(-.*)?$/, ([, scale]) => `cursor-pointer transition active:scale${scale || '-95'}`],
+
+    ['pr', 'relative'],
+    ['pa', 'absolute'],
+    ['pf', 'fixed'],
+    ['ps', 'sticky'],
+
+    // position layout
+    ['pxc', 'pa left-1/2 -translate-x-1/2'],
+    ['pyc', 'pa top-1/2 -translate-y-1/2'],
+    ['pcc', 'pxc pyc'],
+
+    // flex layout
+    ['fcc', 'flex justify-center items-center'],
+    ['fccc', 'fcc flex-col'],
+    ['fxc', 'flex justify-center'],
+    ['fyc', 'flex items-center'],
+    ['fs', 'flex justify-start'],
+    ['fsc', 'flex justify-start items-center'],
+    ['fse', 'flex justify-start items-end'],
+    ['fe', 'flex justify-end'],
+    ['fec', 'flex justify-end items-center'],
+    ['fb', 'flex justify-between'],
+    ['fbc', 'flex justify-between items-center'],
+    ['fa', 'flex justify-around'],
+    ['fac', 'flex justify-around items-center'],
+    ['fw', 'flex justify-wrap'],
+    ['fwr', 'flex justify-wrap-reverse']
+  ]
 })
